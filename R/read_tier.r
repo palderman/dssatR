e721b6e169cfc5e4a08340172ce5ced684dd0ccf
Phillip D. Alderman <p.alderman@cgiar.org>
2014-05-20 21:27:50 -0700

    Bugfix

--- R/read_tier.r
+++ R/read_tier.r
@@ -3,17 +3,21 @@ read.tier <- function(header,l1,nrows,file.name,fmt.list=NULL){
     cnames = strsplit(gsub('@','',header),split='  *')[[1]]
     cnames = cnames[cnames!='']
     pos = vector(length=length(cnames),mode='list')
-    widths = vector(length=length(cnames),mode='numeric')
-    for(i in 1:length(cnames)){
-       tmp = str.index(header,cnames[i])
-       widths[i]=tmp$stop-tmp$start+1
-    }
     fmt = vector(length=length(cnames),mode='character')
     if(is.null(fmt.list)) fmt.list = fmt.default()
     for(i in 1:length(fmt.list)){
         fmt[cnames%in%fmt.list[[i]]]=names(fmt.list)[i]
     }
     class = fmt2class(fmt)
+    if(all(cnames%in%unlist(fmt.list))){
+        widths = fmt2widths(fmt)
+    }else{
+        widths = vector(length=length(cnames),mode='numeric')
+        for(i in 1:length(cnames)){
+            tmp = str.index(header,cnames[i])
+            widths[i]=tmp$stop-tmp$start+1
+        }
+    }
     vars = read.fwf(file.name,widths=widths,skip=l1,nrow=nrows,
         colClasses=class,comment.char='!',blank.lines.skip=TRUE,
         na.strings=c('-99','-99.','-99.0'))
