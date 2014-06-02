67603b39e7dc68fa72defaadfefde753a2e0b256
Phillip D. Alderman <p.alderman@cgiar.org>
2014-06-02 11:07:47 -0700

    Added variables to fmt.default() for reading AgMERRA format weather files

--- R/fmt_default.r
+++ R/fmt_default.r
@@ -3,7 +3,7 @@ fmt.default <- function(){
         `%6.0f`=c('HWAM','CWAM','T#AM','H#AM','CWAD','GWAD','LWAD','SWAD',
             'GSTD','WELEV','ELEV','FLAG','REFHT','WNDHT'),
         `%6.1f`=c('H#UM','TMAX','TMIN','RAIN','WIND','TDEW','T2M','RH2M',
-            'TAV','AMP','SRAD'),
+            'TAV','AMP','SRAD','DEWP','VPRS','RHUM'),
         `%6.2f`=c('HWUM'),
         `%6.3f`=c('HIAM'),
         `%6.5i`=c('ADAT','MDAT','IEDAT','DATE'),
