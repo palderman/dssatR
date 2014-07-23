6326e1d839bd722a206459cde426d3bd019070a0
Phillip D. Alderman <p.alderman@cgiar.org>
2014-07-23 14:56:29 -0700

    bugfix

--- R/write_tier.r
+++ R/write_tier.r
@@ -6,9 +6,12 @@ write.tier <- function(tier,file.name,fmt.list=NULL){
         fmt[cnames%in%fmt.list[[i]]]=names(fmt.list)[i]
     }
     class = fmt2class(fmt)
+    fmt[1] = paste('@%-',fmt2width(fmt[1]),'s',sep='')
     for(c in 1:ncol(tier)){
         tier[,c] = sprintf(fmt[c],tier[,c])
+        cnames[c] = sprintf(gsub('\\..*[fi]','s',fmt[c]),cnames[c])
     }
+    tier = rbind(cnames,tier)
     tier = do.call(paste,c(tier,sep=''))
     write(tier,file=file.name,append=TRUE)
     return(invisible(tier))
