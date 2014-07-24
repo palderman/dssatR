a012fdde663c73f95848d7ed6cee79b420f0e8f6
Phillip D. Alderman <p.alderman@cgiar.org>
2014-07-24 15:45:32 -0700

    bugfix

--- R/read_gen.r
+++ R/read_gen.r
@@ -42,44 +42,13 @@ read.gen <- function(file.name,model=NULL,type=NULL){
             file.name=file.name,fmt.list=fmt.list)
         parameters[[i]]=na.omit(params)
     }
-
-#    for (i in 1:length(hlines)){
-#        classes = get.fmt(header[[i]],
-#                      model=rep(model,length(header[[i]])),
-#                      type=rep(type,length(header[[i]])))
-#        spaces = nchar(classes)-nchar(gsub(' *','',classes))
-#        widths = gsub('%-*','',classes)
-#        widths = gsub('[a-z].*','',widths)
-#        widths = gsub('\\..*','',widths)
-#        widths = as.numeric(widths)+spaces
-#        classes = gsub('.*e.*','numeric',classes)
-#        classes = gsub('.*s.*','character',classes)
-#        classes = gsub('.*f.*','numeric',classes)
-#        classes = gsub('.*d.*','numeric',classes)
-#        section=gen[(hlines[i]+1):
-#                     ifelse(i==length(hlines),length(gen),hlines[i+1]-hlines[i])]
-#        section=section[section!='']
-#        section=section[substr(section,1,1)!='!']
-#        params=matrix(ncol=length(widths),nrow=length(section))
-#        pos1=cumsum(c(1,widths[1:(length(widths)-1)]))
-#        pos2=cumsum(widths)
-#        for(c in 1:length(widths)){
-#            params[,c]=substr(section,pos1[c],pos2[c])
-#        }
-#        params=as.data.frame(params)
-#        for(j in 1:ncol(params)){
-#            if(classes[j]=='numeric'){
-#                params[,j]=as.numeric(params[,j])
-#            }else{
-#                params[,j]=gsub(' *','',params[,j])
-#            }
-#        }
-#        parameters[[i]]=na.omit(params)
-#        colnames(parameters[[i]]) = header[[i]]
-#        econum=gsub('  *','',parameters[[i]]$`ECO#`)
-#        parameters[[i]]=
-#            parameters[[i]][econum!='      '&!is.na(econum),]
-#    }
+    parameters = lapply(parameters,function(x){
+        for(c in 1:ncol(x)){
+            if(is.character(x[,c])){
+                x[,c]=gsub('^ *','',gsub(' *$','',x[,c]))
+            }
+        }
+    })
     gen = list(title=title,comments=comments,parameters=parameters)
     return(gen)
 }
