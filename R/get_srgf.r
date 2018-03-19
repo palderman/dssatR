91c0341a73c3d5f945158e2ffb2171a887b8f632
Phillip D. Alderman <phillip.alderman@okstate.edu>
2018-03-19 10:06:35 -0500

    Fixing calculation of center of soil layer within get.srgf

--- R/get_srgf.r
+++ R/get_srgf.r
@@ -1,8 +1,12 @@
 get.srgf <- function(slb){
     srgf = vector(length=length(slb),mode='numeric')
     srgf[] = 1
-    lyr.center = (slb - c(0,slb[1:(length(slb)-1)]))/2
-    lyr.center = slb - lyr.center
+    if(length(slb)>1){
+      lyr.center = (slb - c(0,slb[1:(length(slb)-1)]))/2
+      lyr.center = slb - lyr.center
+    }else{
+      lyr.center = slb/2
+    }
     srgf[lyr.center > 20] = exp(-0.02*lyr.center[lyr.center > 20])
     return(srgf)
 }
