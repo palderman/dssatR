6fee0ea08c2797891c4e57657f13dba0072ad8de
Phillip Alderman <phillip.d.alderman@gmail.com>
2018-05-10 16:13:15 -0500

    Replacing get.sldr() function with a vectorized version

--- R/get_sldr.R
+++ R/get_sldr.R
@@ -1,11 +1,13 @@
 get.sldr <- function(drainage.class){
-    sldr <- switch(drainage.class,
-                   'Excessively drained'=0.85,
-                   'Somewhat excessively drained'=0.75,
-                   'Well drained'=0.60,
-                   'Moderately well drained'=0.40,
-                   'Somewhat poorly drained'=0.25,
-                   'Poorly drained'=0.05,
-                   'Very poorly drained'=0.01)
+    sldr <- vector(mode='numeric',length=length(drainage.class))
+
+    sldr[drainage.class=='Excessively drained']=0.85
+    sldr[drainage.class=='Somewhat excessively drained']=0.75
+    sldr[drainage.class=='Well drained']=0.60
+    sldr[drainage.class=='Moderately well drained']=0.40
+    sldr[drainage.class=='Somewhat poorly drained']=0.25
+    sldr[drainage.class=='Poorly drained']=0.05
+    sldr[drainage.class=='Very poorly drained']=0.01
+
     return(sldr)
 }
